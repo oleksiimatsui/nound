2401043c35566592eb94a815c99d51990f462e6c
Oleksii Matsui <ommatsui@knu.ua>
2024-04-25 12:40:51 +0300

    feat: add file button

--- source/App/MainComponent.h
+++ source/App/MainComponent.h
@@ -7,61 +7,87 @@
 #include "NodeGraph.h"
 #include "GraphProvider.h"
 
-class AppState
+class StretchComponent : public juce::Component
 {
 public:
-    static AppState &getInstance()
+    StretchComponent(bool is_vertical, juce::Component *_A, juce::Component *_B, float proportion) : A(_A), B(_B), resizerBar(&stretchableManager, 1, vertical)
     {
-        static AppState instance;
-        return instance;
+        vertical = is_vertical;
+        addAndMakeVisible(resizerBar);
+        addAndMakeVisible(A);
+        addAndMakeVisible(B);
+        stretchableManager.setItemLayout(0,
+                                         -0.01, -0.9,
+                                         -proportion);
+        stretchableManager.setItemLayout(1,
+                                         5, 5, 5);
+        stretchableManager.setItemLayout(2,
+                                         -0.1, -0.9,
+                                         -(1 - proportion));
     }
-    bool isPlaying() const { return stateTree.getProperty("isPlaying", false); }
-    void setPlaying(bool playing) { stateTree.setProperty("isPlaying", playing, nullptr); }
+
+    void resized() override
+    {
+        auto r = getLocalBounds().reduced(4);
+        Component *comps[] = {A, &resizerBar, B};
+        stretchableManager.layOutComponents(comps, 3,
+                                            r.getX(), r.getY(), r.getWidth(), r.getHeight(),
+                                            vertical, true);
+    }
 
 private:
-    juce::ValueTree stateTree;
-    AppState()
-    {
-        stateTree = juce::ValueTree("Foo");
-        stateTree.setProperty("isPlaying", false, nullptr);
-    }
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AppState)
+    bool vertical;
+    juce::Component *A;
+    juce::Component *B;
+    juce::StretchableLayoutManager stretchableManager;
+    juce::StretchableLayoutResizerBar resizerBar;
 };
 
-class SoundResult final : public juce::Component
+class MainComponent : public juce::Component
 {
 public:
-    SoundResult()
+    MainComponent() : node_editor(g), stretcher(false, &dropdown_panel, &node_editor, 0.1)
     {
-        addAndMakeVisible(&play_button);
+        setTitle("Welcome to nound");
+        setDescription("sound node editor");
+        setFocusContainerType(FocusContainerType::focusContainer);
+        addAndMakeVisible(stretcher);
+
+        file_button.setButtonText("FILE");
+        file_button.onClick = [&]
+        {
+            juce::PopupMenu menu;
+            menu.addItem("New", nullptr);
+            menu.addItem("Open", nullptr);
+            menu.addItem("Save", nullptr);
+            menu.addItem("Export", nullptr);
+            menu.showMenuAsync(juce::PopupMenu::Options{}.withTargetComponent(file_button));
+        };
+
         play_button.setButtonText("PLAY");
         play_button.onClick = [this]
         { play(); };
-        addAndMakeVisible(&stop_button);
         stop_button.setButtonText("STOP");
         stop_button.onClick = [this]
         {
             stop();
         };
-        setSize(300, 100);
+        toolbar.addElements(std::vector<juce::Component *>({&file_button, &play_button, &stop_button}));
+        addAndMakeVisible(toolbar);
+        setSize(500, 500);
+    }
+    ~MainComponent() override
+    {
     }
 
-    void paint(juce::Graphics &g) override
+    void paint(juce::Graphics &g)
     {
-        g.setColour(juce::Colours::black);
-        g.setFont(juce::Font(20.0f));
-        g.drawRoundedRectangle(getLocalBounds().reduced(2).toFloat(), 5.0f, 3.0f);
-    }
-
-    void resized() override
-    {
-        play_button.setBounds(0, 0, 100, 50);
-        stop_button.setBounds(100, 0, 100, 50);
     }
 
     void play()
     {
         auto graph = GraphProvider::getGraph();
+        Data d = nullptr;
         for (auto &[id, n] : graph->getNodes())
         {
             if (graph->getInputConnectionsOfNode(id).size() == 0)
@@ -84,116 +110,57 @@ public:
         player.Stop();
     }
 
+    void resized() override
+    {
+        juce::FlexBox fb;
+        fb.flexDirection = juce::FlexBox::Direction::column;
+
+        fb.items.add(juce::FlexItem(getWidth(), 50, toolbar));
+        juce::FlexItem second_item(getWidth(), 50, stretcher);
+        second_item = second_item.withFlex(1);
+        fb.items.add(second_item);
+
+        fb.performLayout(getLocalBounds());
+    }
+
 private:
-    Data d;
+    SoundOutputSource player;
+    Graph *g = GraphProvider::getGraph();
+    NodeEditorComponent node_editor;
+    DropdownComponent dropdown_panel;
+    StretchComponent stretcher;
     juce::TextButton play_button;
     juce::TextButton stop_button;
-    SoundOutputSource player;
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SoundResult);
-};
+    juce::TextButton file_button;
 
-class NodeEditorWithPanel final : public juce::Component
-{
-public:
-    NodeEditorWithPanel()
+    class Toolbar : public juce::Component
     {
-        GraphProvider::reset();
-        Graph *graph = GraphProvider::getGraph();
-        editor = new NodeEditorComponent(graph);
-        tree = new DropdownComponent();
+    public:
+        Toolbar()
+        {
+        }
+        void addElements(std::vector<juce::Component *> _elements)
+        {
+            elements = _elements;
+            for (auto &e : elements)
+            {
+                addAndMakeVisible(e);
+            }
+        }
+        void resized() override
+        {
+            juce::FlexBox fb;
+            fb.flexDirection = juce::FlexBox::Direction::row;
+            for (auto &e : elements)
+            {
+                fb.items.add(juce::FlexItem(100, getHeight(), *e));
+            }
+            fb.performLayout(getLocalBounds());
+        }
+        std::vector<juce::Component *> elements;
+    };
+    Toolbar toolbar;
+    std::vector<juce::Component *> elements;
 
-        addAndMakeVisible(tree);
-        addAndMakeVisible(resizerBar);
-        addAndMakeVisible(editor);
-
-        stretchableManager.setItemLayout(0,
-                                         -0.05, -0.9,
-                                         -0.1);
-        stretchableManager.setItemLayout(1,
-                                         5, 5, 5);
-        stretchableManager.setItemLayout(2,
-                                         -0.1, -0.9,
-                                         -0.9);
-    }
-
-    ~NodeEditorWithPanel()
-    {
-        delete editor;
-        delete tree;
-    }
-
-    void resized() override
-    {
-        auto r = getLocalBounds().reduced(4);
-        Component *comps[] = {tree, &resizerBar, editor};
-        stretchableManager.layOutComponents(comps, 3,
-                                            r.getX(), r.getY(), r.getWidth(), r.getHeight(),
-                                            false, true);
-    }
-
-private:
-    //   juce::Viewport viewPort;
-    NodeEditorComponent *editor;
-    DropdownComponent *tree;
-    juce::StretchableLayoutManager stretchableManager;
-    juce::StretchableLayoutResizerBar resizerBar{&stretchableManager, 1, true};
-};
-
-enum TransportState
-{
-    Stopped,
-    Starting,
-    Playing,
-    Stopping
-};
-
-class MainComponent : public juce::Component
-{
-public:
-    MainComponent()
-    {
-        setTitle("Welcome to nound");
-        setDescription("sound node editor");
-        setFocusContainerType(FocusContainerType::focusContainer);
-
-        addAndMakeVisible(sound_result);
-        addAndMakeVisible(resizerBar);
-        addAndMakeVisible(node_editor);
-
-        stretchableManager.setItemLayout(0,
-                                         -0.01, -0.9,
-                                         -0.1);
-        stretchableManager.setItemLayout(1,
-                                         5, 5, 5);
-        stretchableManager.setItemLayout(2,
-                                         -0.1, -0.9,
-                                         -0.9);
-
-        setSize(500, 500);
-    }
-    ~MainComponent() override
-    {
-    }
-
-    void paint(juce::Graphics &g)
-    {
-    }
-    void resized() override
-    {
-        auto r = getLocalBounds().reduced(4);
-
-        Component *comps[] = {&sound_result, &resizerBar, &node_editor};
-
-        stretchableManager.layOutComponents(comps, 3,
-                                            r.getX(), r.getY(), r.getWidth(), r.getHeight(),
-                                            true, true);
-    }
-
-private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
-    SoundResult sound_result;
-    NodeEditorWithPanel node_editor;
-    juce::StretchableLayoutManager stretchableManager;
-    juce::StretchableLayoutResizerBar resizerBar{&stretchableManager, 1, false};
-    juce::Random random;
 };
